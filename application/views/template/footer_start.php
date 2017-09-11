ffa9ded2b66d0a507ac927541e9599bd2b4322d0
vaishali-s-tijge <31863786+vaishali-s-tijge@users.noreply.github.com>
2017-09-12 01:01:48 +0530

    template files

---
+++ application/views/template/footer_start.php
@@ -0,0 +1,20 @@
+<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+
+</div>
+
+
+<!-- jQuery 2.1.4 -->
+<script src="<?php echo base_url();?>assets/js/jquery-2.1.4.min.js"></script>
+<!-- jQuery UI 1.11.4 -->
+<link rel="stylesheet" href="<?=base_url();?>assets/js/jquery-ui-1.11.0.custom/jquery-ui.min.css">
+<script src="<?=base_url();?>assets/js/jquery-ui-1.11.0.custom/jquery-ui.min.js" type="text/javascript"></script>
+
+<!-- Bootstrap JS -->
+<script src="<?php echo base_url();?>assets/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="<?php echo base_url();?>assets/js/custom_grid_plugin.1.0.js" type="text/javascript"></script>
+
+<style type="text/css">
+    .ui-datepicker-header{  
+        color:#666666!important;  
+    }  
+</style>
---
+++ application/views/template/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Codaemon Assignment</title>
+	<!-- Bootstrap 3.3.4 -->
+	<link href="<?php echo base_url();?>assets/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+	<!-- FontAwesome 4.3.0 -->
+	<link href="<?php echo base_url();?>assets/bootstrap/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+
+	<!-- Web Application Style Sheet -->
+	<link rel="stylesheet" href="<?php echo base_url();?>assets/css/style.css" />
+
+	<script type="text/javascript">var SITEROOT = '<?php echo base_url();?>';</script>
+
+</head>
+<body>
+
+<div id="container">
